3b3dcb597ae95ef71cefb87c5a686aa85a7b0084
SzymonMakula <szymakula@gmail.com>
2023-07-03 15:02:48 +0200

    next save

--- plot_tools/tsp_plot.py
+++ plot_tools/tsp_plot.py
@@ -16,8 +16,8 @@ if len(sys.argv) == 3:
     if sys.argv[2] == "c":
         chosen_algorithm = "christofides"
 
-problem_pathname = f"../tsp/datasets/problems/{problem_name}.tsp"
-result_pathname = f"../../dist/tsp/{chosen_algorithm}/{problem_name}.json"
+problem_pathname = f"../datasets/tsp/problems/{problem_name}.tsp"
+result_pathname = f"../dist/tsp/{chosen_algorithm}/{problem_name}.json"
 
 result_data = open(result_pathname)
 json_data = json.load(result_data)
@@ -28,10 +28,16 @@ problem = tsplib95.load(problem_pathname)
 G = problem.get_graph()
 sol = nx.algorithms.approximation.christofides(G)
 
-print(sys.argv[1])
 
 pos = nx.get_node_attributes(G, "coord")
 edge_list = list(nx.utils.pairwise(tour))
+
+plt.figure(1)
+plt.ylabel('Współrzędne Y')
+plt.xlabel('Współrzędne X')
+plt.axis("on")
+plt.title("Aproksymacja optymalnej ścieżki")
+
 
 nx.draw_networkx(
     G,
@@ -42,5 +48,19 @@ nx.draw_networkx(
     node_size=2,
     width=1,
 )
+
+if json_data["data_set"]["opt_tour"] is not None:
+    plt.figure(2)
+    opt_tour = json_data["data_set"]["opt_tour"]
+    opt_edge_list = list(nx.utils.pairwise(opt_tour))
+    nx.draw_networkx(
+        G,
+        pos,
+        edgelist=opt_edge_list,
+        with_labels=False,
+        edge_color="green",
+        node_size=2,
+        width=1,
+    )
 
 plt.show()
